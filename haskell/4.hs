cc81e8c6eaf00fe785a8933d05cec0bb37b1b3fd
Grzegorz Antoniak <ga@anadoxin.org>
2014-12-23 20:55:34 +0100

    solved problem #4 in haskell

---
+++ haskell/4.hs
@@ -0,0 +1,61 @@
+decomp :: Int -> [Int]
+decomp x = reverse $ decompInner [] x
+
+decompInner :: [Int] -> Int -> [Int]
+decompInner a 0 = a
+decompInner a x = a ++ [x `mod` 10] ++ decompInner a (x `div` 10)
+
+buildp :: Int -> [Int]
+buildp x = r ++ (reverse r)
+	where
+		r = decomp x
+
+encomp :: [Int] -> Int
+encomp a = (encompInner (reverse a) 10) `div` 10
+
+encompInner :: [Int] -> Int -> Int
+encompInner (x:xs) d = x * d + encompInner xs (d * 10)
+encompInner [] _ = 0
+
+buildpnum :: Int -> Int
+buildpnum x = encomp $ buildp x
+
+buildplist :: [Int] -> [Int]
+buildplist a = map buildpnum a
+
+searchp :: [Int] -> [[Int]]
+searchp a = map fermat (reverse (buildplist a))
+
+searchm :: [[Int]]
+searchm = filter (\ factors -> length (decomp (factors !! 0)) == 3 && length (decomp (factors !! 1)) == 3) $ searchp [100..999]
+
+-- Integer square root, ceiling the partial result.
+isqrtc :: Int -> Int
+isqrtc x = ceiling $ sqrt $ fromIntegral x
+
+-- Fermat factorization gateway function. Real Fermat algorithm is in
+-- `fermatGen`.
+fermat :: Int -> [Int]
+fermat 0 = []
+fermat modulus
+    -- Skip even moduli.
+    | modulus `mod` 2 == 0 = [modulus `div` 2, 2]
+    | otherwise            = fermatGen modulus $ isqrtc modulus
+
+-- Fermat factorization:
+--
+-- You increase `a` by 1 until `a*a-N` = `b*b`.
+-- Start with `a=ceil(sqrt(modulus))`.
+--
+-- When `until` is reached, factors are: [a-b, a+b].
+--
+fermatGen :: Int -> Int -> [Int]
+fermatGen modulus a
+    -- End conditional.
+    | b * b == a * a - modulus            = [a - b, a + b]
+    -- Loop.
+    | otherwise                           = fermatGen modulus $ a + 1
+    where
+        b = isqrtc(a * a - modulus);
+
+main = putStrLn(show $ (show (searchm !! 0 !! 0)) ++ " x " ++ (show (searchm !! 0 !! 1)))
