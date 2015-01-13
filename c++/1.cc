adea2876ffa4ce8be0c302338e55848ed963a257
Grzegorz Antoniak <ga@anadoxin.org>
2015-01-13 19:04:56 +0100

    c++#1

---
+++ c++/1.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+
+template <typename T, T ParametrizedValue>
+class Modulator {
+public:
+    T modulo(T input) {
+        return input % ParametrizedValue;
+    }
+};
+
+typedef Modulator<int, 3> Modulator3;
+typedef Modulator<int, 5> Modulator5;
+
+template <typename T, T ParametrizedValue>
+class Comparator {
+public:
+    bool compare(T input) {
+        return input == ParametrizedValue;
+    }
+};
+
+typedef Comparator<int, 0> Comparat0r;
+
+int main() {
+    Modulator3 by3;
+    Modulator5 by5;
+    Comparat0r with0;
+
+    size_t sum = 0;
+    for(int i = 0; i < 1000; ++i) {
+        // Yoda expression
+        if(with0.compare(by3.modulo(i)) || with0.compare(by5.modulo(i))) {
+            sum += i;
+        }
+    }
+
+    std::cout << sum << "\n";
+}
