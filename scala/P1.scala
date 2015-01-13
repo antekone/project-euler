3de263af39dbe5554b9d003ae3f3819d7604401d
Grzegorz Antoniak <ga@anadoxin.org>
2015-01-13 18:22:35 +0100

    scala#1

---
+++ scala/P1.scala
@@ -0,0 +1,15 @@
+// project euler #1 in scala by antekone
+// http://anadoxin.org/blog
+//
+// usage:
+//
+// $ scalac P1.scala
+// $ scala P1
+
+object P1 extends App {
+	def nums(upTo: Int): List[Int] =
+		for(i <- List.range(0, upTo) if i % 3 == 0 || i % 5 == 0)
+			yield i
+
+	println(nums(1000).foldLeft(0)(_ + _));
+}
