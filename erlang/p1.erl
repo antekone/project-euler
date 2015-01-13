a6008e719686f60fe1f84704f95fa989133b873c
Grzegorz Antoniak <ga@anadoxin.org>
2015-01-13 18:49:30 +0100

    fix indent

--- erlang/p1.erl
+++ erlang/p1.erl
@@ -10,7 +10,7 @@
 
 problem1_gen(UpTo) -> lists:foldl(fun(Z, Sum) ->
                                       Z + Sum
-                              end, 0, [X || X <- lists:seq(0, UpTo), (X rem 3 =:= 0) or (X rem 5 =:= 0)]).
+                                  end, 0, [X || X <- lists:seq(0, UpTo), (X rem 3 =:= 0) or (X rem 5 =:= 0)]).
 
 problem1() -> io:fwrite("~p\n", [problem1_gen(999)]),
               init:stop().
