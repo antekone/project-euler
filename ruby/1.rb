f27daca94b17ddb61ffea5bd6f80fbd8bb4c79ef
Grzegorz Antoniak <ga@anadoxin.org>
2015-01-21 20:35:20 +0100

    ruby#1 converted to functional style

--- ruby/1.rb
+++ ruby/1.rb
@@ -1,14 +1,8 @@
-# project euler #1 in ruby by antekone
+# Project Euler #1 in Ruby, by antekone
 # http://anadoxin.org/blog
 #
 # usage:
 #
 # $ ruby 1.rb
 
-sum = 0
-
-1000.times do |i|
-    sum += i if i % 3 == 0 or i % 5 == 0
-end
-
-puts(sum)
+puts (0...1000).reject { |i| !(i % 3 == 0 or i % 5 == 0) }.inject(0) { |i,j| i + j }
