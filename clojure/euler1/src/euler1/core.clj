057712dd175e55d9ec22cecd218fb7c75350e3fa
Grzegorz Antoniak <antek1@zoho.com>
2015-01-22 17:11:33 +0100

    clojure#1 formatting fixes

--- clojure/euler1/src/euler1/core.clj
+++ clojure/euler1/src/euler1/core.clj
@@ -12,27 +12,25 @@
 
 (ns euler1.core (:gen-class))
 
-(defn make-sum
-  [x y]
-  (+ x y))
+(defn make-sum [x y] (+ x y))
 
-(defn make-filter
-  [x]
-  (or
-    (= 0
-       (mod x 3))
-    (= 0
-       (mod x 5))))
+(defn make-filter [x]
+  (or (= 0 (mod x 3))
+      (= 0 (mod x 5))))
 
-(defn euler-1
-  []
+(defn euler-1 []
   (reduce make-sum (filter make-filter (range 0 1000))))
 
 ; alternative, shorter version:
 
-(defn s1
-  []
-  (reduce (fn [x y] (+ x y)) (filter (fn [x] (or (= 0 (mod x 3)) (= 0 (mod x 5)))) (range 0 1000))))
+(defn s1 []
+  (reduce
+    (fn [x y] (+ x y))
+    (filter
+      (fn [x]
+        (or (= 0 (mod x 3))
+            (= 0 (mod x 5))))
+      (range 0 1000))))
 
 ; reload function, used from REPL
 (defn -r [] (use 'euler1.core :reload))
