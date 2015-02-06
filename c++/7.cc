1f7f355b1f7de292f438ebe26e96c186d7fe0537
Grzegorz Antoniak <antek1@zoho.com>
2015-02-06 17:35:43 +0100

    c++#2

---
+++ c++/7.cc
@@ -0,0 +1,44 @@
+// Project Euler #7 in C++ by antekone
+// http://anadoxin.org/blog
+//
+// Usage:
+//
+// $ g++ 7.cc -o 7
+// $ ./7 | grep 10001
+
+#include <vector>
+#include <iostream>
+#include <math.h>
+
+// Sieve of Erastothenes.
+
+void e_sieve(size_t n) {
+	std::vector<bool> A;
+	A.resize(n);
+	std::fill(A.begin(), A.end(), true);
+
+	size_t sqrtn = sqrt(n);
+	for(size_t i = 2; i < sqrtn; ++i) {
+		size_t j = i * i;
+		if(! A[i])
+			continue;
+
+		while(j < n) {
+			A[j] = false;
+			j += i;
+		}
+	}
+
+	size_t cnt = 1;
+	for(size_t i = 2; i < n; ++i) {
+		if(A[i]) {
+			std::cout << "idx=" << cnt << " prime=" << i << "\n";
+			++cnt;
+		}
+	}
+}
+
+int main() {
+	e_sieve(211000);
+	return 0;
+}
