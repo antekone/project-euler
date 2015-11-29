662915cec97f92ecb1bc48c73d43496acb3d6732
Grzegorz Antoniak <ga@anadoxin.org>
2015-11-29 21:38:30 +0100

    #3 in Scala update

--- scala/P3.scala
+++ scala/P3.scala
@@ -7,52 +7,43 @@ import scala.annotation.tailrec
 import scala.collection.mutable.Queue
 
 object P3 {
-  def max(t: (Long,Long)): Long = if(t._1 > t._2) t._1 else t._2
-  def min(t: (Long,Long)): Long = if(t._1 > t._2) t._2 else t._1
+  // oneliners
+  def max(t: (Long,Long)): Long =    if(t._1 > t._2) t._1 else t._2
+  def min(t: (Long,Long)): Long =    if(t._1 > t._2) t._2 else t._1
+  def fermat(n: Long): (Long,Long) = if(n % 2 == 0) (2, n / 2) else fermatGen(n, math.sqrt(n).toLong)
+  def biggestFactor(n: Long): Long = explodeFactorsGen(Seq(n), Seq())._2.max
 
+  // tail-recursive functions
   @tailrec
   def fermatGen(n: Long, a: Long): (Long,Long) = {
     val f = a * a - n
     val b = math.sqrt(f).toLong
 
-    if(b * b == f) {
+    if(b * b == f)
       (a - b, a + b)
-    } else {
+    else
       fermatGen(n, a + 1)
-    }
   }
 
-  def fermat(n: Long): (Long,Long) = {
-    if(n % 2 == 0)
-      (2, n / 2)
-    else {
-      fermatGen(n, math.sqrt(n).toLong)
-    }
+  @tailrec
+  def explodeFactorsGen(arg: (Seq[Long], Seq[Long])): (Seq[Long], Seq[Long]) = {
+    if(arg._1.isEmpty)
+      return arg
+
+    val result = arg._2
+    val f = fermat(arg._1.head)
+
+    if(min(f) == 1)
+      // head is prime
+      explodeFactorsGen((arg._1.tail, result :+ max(f)))
+    else
+      // head is not prime
+      explodeFactorsGen((arg._1.tail :+ f._1 :+ f._2, result))
   }
 
-  def explodeFactors(seq: Seq[Long], outseq: Queue[Long]): Unit = {
-    seq.foreach(x => {
-      val f = fermat(x)
-      val isprime = min(f) == 1
-
-      if(isprime) {
-        outseq += max(f)
-      } else {
-        explodeFactors(Seq(f._1, f._2), outseq)
-      }
-    })
-  }
-
-  def biggestFactor(n: Long): Long = {
-    val ft = fermat(n)
-    val outseq = new Queue[Long]()
-
-    explodeFactors(Seq(ft._1, ft._2), outseq)
-    outseq.max
-  }
-
+  // normal functions
   def main(args: Array[String]) {
-    val biggestTestFactor = biggestFactor(600851475143L)
-    println(biggestTestFactor)
+    println(biggestFactor(13195L))
+    println(biggestFactor(600851475143L))
   }
 }
