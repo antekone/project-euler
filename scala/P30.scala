1343361d09d60f462a66a64b9d5443bf1d816041
Grzegorz Antoniak <ga@anadoxin.org>
2019-02-16 22:04:18 +0100

    scala #30 smaller

--- scala/P30.scala
+++ scala/P30.scala
@@ -1,29 +1,3 @@
-import scala.collection.mutable
-
-class Main {
-  def p30(): Unit = {
-    var n = 2
-    var expr = true
-    var mem = new mutable.MutableList[Int]
-
-    while(expr) {
-      val powSum = n.toString().map(_.toInt - 0x30).map(n => n*n*n*n*n).sum
-      if(powSum == n) {
-        mem += n
-      }
-
-      n += 1
-      if(n > 9999999) {
-        expr = false
-      }
-    }
-
-    println(mem.sum)
-  }
-}
-
-object Main {
-  def main(args: Array[String]): Unit = {
-    new Main().p30()
-  }
+object Main extends App {
+  println(Range(2, 9999999).filter(n => n.toString.map(_.toInt - 0x30).map(math.pow(_, 5)).sum == n).sum)
 }
